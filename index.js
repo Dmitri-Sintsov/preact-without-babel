cacf6819ed1e396de32b3a8dc06c252b7f382985
Dmitriy Sintsov <questpc256@gmail.com>
2024-09-11 13:44:28 +0300

    Use es6 modules. Use htm library for rendering.

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
-'use strict';
+import { h, Component, render } from 'https://unpkg.com/preact?module';
+import htm from 'https://unpkg.com/htm?module';
 
-const { Component, h, render } = window.preact;
+const html = htm.bind(h);
 
 
 /** Example classful component */
 class App extends Component {
 	componentDidMount() {
-		this.setState({ message:'Hello!' });
+		this.setState({ message: 'Hello!' });
 	}
 	render(props, state) {
-		return (
-			h('div', {id:'app'},
-				h(Header, { message: state.message }),
-				h(Main)
-			)
-		);
+		return html`
+            <div id="app">
+                <${Header} message="${state.message}"/>
+                <${Main} />
+            </div>
+        `;
 	}
 }
 
 
 /** Components can just be pure functions */
 const Header = (props) => {
-	return h('header', null,
-		h('h1', null, 'App'),
-		props.message && h('h2', null, props.message)
-	);
+	return html`
+	    <header>
+	        <h1>App</h1>
+            <h2>${props.message}</h2>
+	    </header>
+	`;
 };
 
 
-/** Instead of JSX, use: h(type, props, ...children) */
+/** Instead of JSX, use: htm */
 class Main extends Component {
 	render() {
 		const items = [1,2,3,4,5].map( (item) => (
-			h('li', {id:item}, 'Item '+item)
+			html`<li id="${item}">Item ${item}</li>`
 		));
-		return (
-			h('main', null,
-				h('ul', null, items)
-			)
-		);
+		return html`
+            <main>
+                <ul>${items}</ul>
+            </main>
+        `;
 	}
 }
 
